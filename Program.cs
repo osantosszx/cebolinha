d67b8e44d5ecba3771e7316dbd17ced7ca17e3c0
guilherme <guiss250507@gmail.com>
2025-09-26 20:07:23 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseDigitada, Ceboles;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!;
+
+Ceboles = fraseDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(Ceboles);
